6a7ed61816d13ceec1fc7eefcfa07dd09a00dbeb
BryanRivasDev <ing.bryanrivasq@gmail.com>
2026-07-14 15:11:00 -0600

    Eliminar enlaces de Acceso Administrativo y Soporte Técnico del footer

--- includes/footer.php
+++ includes/footer.php
@@ -2,8 +2,6 @@
     <div class="container footer-content">
         <div class="footer-links">
             <a href="index.php">Inicio</a>
-            <a href="admin/login.php">Acceso Administrativo</a>
-            <a href="#">Soporte Técnico</a>
         </div>
         <p class="footer-text">&copy; <?php echo date('Y'); ?> Soporte Master. Todos los derechos reservados. Desarrollado con PHP & MySQL.</p>
     </div>
